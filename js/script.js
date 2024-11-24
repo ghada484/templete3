7e648a590eac3390ce0f3ae242e37ef981ce8c6a
ghadaabdalla <saadabdalla1919@gmail.com>
2024-11-24 19:25:23 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+        // document.getElementById('navbar-toggler').addEventListener('click', function () {
+        //   const navContent = document.getElementById('navbarSupportedContent');
+        //   const isExpanded = this.getAttribute('aria-expanded') === 'true';
+      
+        //   if (isExpanded) {
+        //     navContent.classList.remove('show'); // يخفي القائمة إذا كانت ظاهرة
+        //     this.setAttribute('aria-expanded', 'false');
+        //   }
+        // });
